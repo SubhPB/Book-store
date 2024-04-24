94af3ca49af73cda14349fd57a55c1495b59f156
SubhPB <harvirbuttar2917@gmail.com>
2024-04-24 11:22:11 -0700

    configured settings

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -1,26 +1,20 @@
-
+import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-tvnf*yw6a8_lde9t=3fn&7uv65_2r4w!mzs^@7ngp(n81az49h'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
 
-# Application definition
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
 
 EXTERNAL_APPS = [
-    'users.apps.UsersConfig'
+    'users.apps.UsersConfig', 
+    'pages.apps.PagesConfig',
 ]
 
 INSTALLED_APPS = [
@@ -49,7 +43,9 @@ ROOT_URLCONF = 'bookstore_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join( BASE_DIR, 'templates' )
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -104,13 +100,16 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_FINDERS = [
+    # FileSystemFinder looks within STATICFILES_DIRS, which set to static.
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    # AppDirectoriesFinder looks for any directories(folder) named 'static' located within app. 'books/static'
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder"
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
